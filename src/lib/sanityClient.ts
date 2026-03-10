1e1e05ef1e08e3de0d47c66ccce6c460e4d3ac24
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2026-03-10 12:52:53 +0100

    add sanity key

--- src/lib/sanityClient.ts
+++ src/lib/sanityClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from "@sanity/client";
 
 export const client = createClient({
-  projectId: "nruc5zuq",
+  projectId: "bhdle05g",
   dataset: "production",
   apiVersion: "2025-06-18",
   useCdn: true,
