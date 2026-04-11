dec9e28654bd7065adf11e1fcfd4d852e0c20eb5
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2026-04-11 16:02:48 +0200

    add pages

--- src/app/[lang]/om oss/page.tsx
+++ src/app/[lang]/om oss/page.tsx
@@ -1,37 +1,37 @@
-import ProjectPageContent from "./ProjectsPageContent";
-import { Metadata } from "next";
-import { getProjectPageMetadata } from "@/lib/metadata/projectMetadata";
+// import ProjectPageContent from "./ProjectsPageContent";
+// import { Metadata } from "next";
+// import { getProjectPageMetadata } from "@/lib/metadata/projectMetadata";
 
-export async function generateMetadata(): Promise<Metadata> {
-  const data = await getProjectPageMetadata();
+// export async function generateMetadata(): Promise<Metadata> {
+//   const data = await getProjectPageMetadata();
 
-  return {
-    title: data.title,
-    description: data.description,
-    keywords: data.keywords,
-    openGraph: {
-      title: data.title,
-      description: data.description,
-      images: data.ogImage
-        ? [
-            {
-              url: data.ogImage.url,
-              alt: data.ogImage.alt,
-              width: 1200,
-              height: 630,
-            },
-          ]
-        : [],
-      siteName: "THB Digital",
-      locale: "no_NO",
-      type: "website",
-      url: "https://thbdigital.no/",
-    },
-  };
-}
+//   return {
+//     title: data.title,
+//     description: data.description,
+//     keywords: data.keywords,
+//     openGraph: {
+//       title: data.title,
+//       description: data.description,
+//       images: data.ogImage
+//         ? [
+//             {
+//               url: data.ogImage.url,
+//               alt: data.ogImage.alt,
+//               width: 1200,
+//               height: 630,
+//             },
+//           ]
+//         : [],
+//       siteName: "THB Digital",
+//       locale: "no_NO",
+//       type: "website",
+//       url: "https://thbdigital.no/",
+//     },
+//   };
+// }
 
-const ProjectsPage = async () => {
-  return <ProjectPageContent />;
-};
+// const ProjectsPage = async () => {
+//   return <ProjectPageContent />;
+// };
 
-export default ProjectsPage;
+// export default ProjectsPage;
--- src/app/[lang]/tjeneste/page.tsx
+++ src/app/[lang]/tjeneste/page.tsx
@@ -1,37 +1,37 @@
-import { Metadata } from "next";
-import { getServicesPageMetadata } from "@/lib/metadata/servicesMetadata";
-import ServicesPageContent from "./ServicesPageContent";
+// import { Metadata } from "next";
+// import { getServicesPageMetadata } from "@/lib/metadata/servicesMetadata";
+// import ServicesPageContent from "./ServicesPageContent";
 
-export async function generateMetadata(): Promise<Metadata> {
-  const data = await getServicesPageMetadata();
+// export async function generateMetadata(): Promise<Metadata> {
+//   const data = await getServicesPageMetadata();
 
-  return {
-    title: data.title,
-    description: data.description,
-    keywords: data.keywords,
-    openGraph: {
-      title: data.title,
-      description: data.description,
-      images: data.ogImage
-        ? [
-            {
-              url: data.ogImage.url,
-              alt: data.ogImage.alt,
-              width: 1200,
-              height: 630,
-            },
-          ]
-        : [],
-      siteName: "THB Digital",
-      locale: "no_NO",
-      type: "website",
-      url: "https://thbdigital.no/",
-    },
-  };
-}
+//   return {
+//     title: data.title,
+//     description: data.description,
+//     keywords: data.keywords,
+//     openGraph: {
+//       title: data.title,
+//       description: data.description,
+//       images: data.ogImage
+//         ? [
+//             {
+//               url: data.ogImage.url,
+//               alt: data.ogImage.alt,
+//               width: 1200,
+//               height: 630,
+//             },
+//           ]
+//         : [],
+//       siteName: "THB Digital",
+//       locale: "no_NO",
+//       type: "website",
+//       url: "https://thbdigital.no/",
+//     },
+//   };
+// }
 
-const ServicesPage = async () => {
-  return <ServicesPageContent />;
-};
+// const ServicesPage = async () => {
+//   return <ServicesPageContent />;
+// };
 
-export default ServicesPage;
+// export default ServicesPage;
