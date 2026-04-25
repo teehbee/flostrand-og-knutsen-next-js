93032c8bb5957844bfbe463f74b868a72f6b13c6
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2026-04-25 12:47:45 +0200

    buxfixes after changing interface file

--- src/components/reusable/imageAndText/TextAndImageAlternate.tsx
+++ src/components/reusable/imageAndText/TextAndImageAlternate.tsx
@@ -17,19 +17,19 @@ export const TextAndImageAlternate: React.FC<ImageAndTextAlternateProps> = ({ co
           {content.tiles.map((tile, index) => (
             <div key={index} className="row tile-row align-items-stretch h-lg-400 mb-30 mb-lg-75">
               <div className="col-12 col-lg-5 d-flex mb-30 mb-lg-0">
-                <Image className="img-cover border-radius-5" width={530} height={400} src={tile.bilde?.asset?.url || "/assets/img/placeholder/20240307_110601.jpg"} alt={tile.bilde?.alt || "placeholder"}></Image>
+                <Image className="img-cover border-radius-5" width={530} height={400} src={tile.image?.asset?.url || "/assets/img/placeholder/20240307_110601.jpg"} alt={tile.bilde?.alt || "placeholder"}></Image>
               </div>
               <div className="col-12 col-lg-7 h-100">
                 <div className="p-30 p-lg-75 alt-bg-color border-radius-5 h-100 d-flex flex-column justify-content-center">
-                  <h3 className="mb-15">{tile.tittel}</h3>
-                  <div className="mb-15">{tile.undertittel}</div>
+                  <h3 className="mb-15">{tile.title}</h3>
+                  <div className="mb-15">{tile.subTitle}</div>
                   <div className="mb-15">
-                    <PortableText value={tile.tekstinnhold} />
+                    <PortableText value={tile.textContent} />
                   </div>
-                  {tile.lenketekst && tile.lenkedestinasjon && (
+                  {tile.linkText && tile.linkDestination && (
                     <div>
-                      <a className="btn btn-primary" href={getLocalizedPath(language, tile.lenkedestinasjon)} target="_blank" rel="noopener noreferrer">
-                        {tile.lenketekst}
+                      <a className="btn btn-primary" href={getLocalizedPath(language, tile.linkDestination)} target="_blank" rel="noopener noreferrer">
+                        {tile.linkText}
                       </a>
                     </div>
                   )}
