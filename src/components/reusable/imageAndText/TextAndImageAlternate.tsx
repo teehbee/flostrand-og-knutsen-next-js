91fedc940fd4eed13ed289cbcd76d6d4a5983be2
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2026-04-25 13:14:05 +0200

    minor fix

--- src/components/reusable/imageAndText/TextAndImageAlternate.tsx
+++ src/components/reusable/imageAndText/TextAndImageAlternate.tsx
@@ -17,7 +17,7 @@ export const TextAndImageAlternate: React.FC<ImageAndTextAlternateProps> = ({ co
           {content.tiles.map((tile, index) => (
             <div key={index} className="row tile-row align-items-stretch h-lg-400 mb-30 mb-lg-75">
               <div className="col-12 col-lg-5 d-flex mb-30 mb-lg-0">
-                <Image className="img-cover border-radius-5" width={530} height={400} src={tile.image?.asset?.url || "/assets/img/placeholder/20240307_110601.jpg"} alt={tile.bilde?.alt || "placeholder"}></Image>
+                <Image className="img-cover border-radius-5" width={530} height={400} src={tile.image?.asset?.url || "/assets/img/placeholder/20240307_110601.jpg"} alt={tile.image?.alt || "placeholder"}></Image>
               </div>
               <div className="col-12 col-lg-7 h-100">
                 <div className="p-30 p-lg-75 alt-bg-color border-radius-5 h-100 d-flex flex-column justify-content-center">
